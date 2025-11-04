f6faf356ba67bfbf316f06bd374d15ee3318484e
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-11-04 11:25:39 -0500

    feat: port Eli Benderesky's example to workable version
    website:
    
    https://eli.thegreenplace.net/2010/01/02/top-down-operator-precedence-parsing
    
    The as-is version snags on an error (Python 3.13), and so I had to
    update the syntax a little bit to get it to at least run. So far so
    good, though.

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((python-base-mode
+  . ((eglot-workspace-configuration
+      . (:basedpyright.analysis (:inlayHints (:callArgumentNames :json-false
+                                                                 :functionReturnTypes :json-false
+                                                                 :variableTypes :json-false)))))))
