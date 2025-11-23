dc8f5f3d8977f93530cd21e9cc3769c69d247ba3
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-11-23 11:10:16 -0500

    feat: make FILENAME the sole required argument
    This is for now.

--- src/pratt_calc/__init__.py
+++ src/pratt_calc/__init__.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 import typer
 
 from pratt_calc.main import evaluate
@@ -9,9 +11,11 @@ def app():
     # Note that the name of this particular function is insigificant,
     # as the function only serves to wrap the logic used by
     # 'typer.run'
-    def cli(exp: str):
-        """Evaluate EXP as an arithmetic expression."""
+    def cli(filename: Annotated[str, typer.Argument(help="Path to source file")]):
+        """Pratt Calc application."""
+        with open(filename, encoding="utf-8") as f:
+            code = f.read()
 
-        print(evaluate(exp))
+            print(evaluate(code))
 
     typer.run(cli)
