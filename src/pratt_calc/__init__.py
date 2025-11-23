af9df81490dab0b1078c94dad1ed29d9be968d9d
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-11-23 10:56:51 -0500

    docs: add comment explaining role of 'cli' function

--- src/pratt_calc/__init__.py
+++ src/pratt_calc/__init__.py
@@ -6,6 +6,9 @@ from pratt_calc.main import evaluate
 def app():
     """Entry-point for project script."""
 
+    # Note that the name of this particular function is insigificant,
+    # as the function only serves to wrap the logic used by
+    # 'typer.run'
     def cli(exp: str):
         """Evaluate EXP as an arithmetic expression."""
 
